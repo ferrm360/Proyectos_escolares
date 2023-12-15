5347f1817035b968b1490e3045ec5d3980978506
neco3 <neco3@192.168.56.1>
2023-12-15 08:53:40 -0600

    Correcion Random Function

--- src/algoritmosrt/AlgoritmoSRT.java
+++ src/algoritmosrt/AlgoritmoSRT.java
@@ -13,7 +13,6 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class AlgoritmoSRT extends JFrame {
     private DefaultTableModel tableModel;
